10599cf4abc91a73295d38b6b93bb2d7fa05a8cc
vbomfim0907 <vbomfim0907@gmail.com>
2026-09-17 13:28:16 +0000

    feat: adicionar página de serviços e estilização básica

--- 3-TRIMESTRE/publico/servidor-02.js
+++ 3-TRIMESTRE/publico/servidor-02.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import path from 'path'
-
+const dirBase = import.meta.dirname
 const app = express()
 const porta = 3000
 //Usando middleware (software intermediario)TODOS OS ARQ ESTATICOS
-app.use(express.static(path.join(import.meta.dirname,'publico')))
+app.use(express.static(path.join( dirBase,'publico')))
 
 //Criar as rotas do servidor
 app.get('/',(req, res) => {
     res.sendFile('/paginas/index.html',{root: import.meta.dirname})
 })
+//Liberar a porta do meu computador
+app.listen(porta, () =>{console.log(`Servidor esta vivo`)})
